1a4de13452f6f25b2f2e5488bbfd7895adeb9eb9
Nowath <83205731+Nowath@users.noreply.github.com>
2024-10-22 00:31:13 +0700

    NavbarEvent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,19 @@ function Navbar() {
     function HandelClick() {
         setOpenNav(!openNav);
     }
+    const NavbarList = [
+        {text:"Something",link:"/about"},
+        {text:"Something",link:"/"},
+        {text:"Something",link:"/about"},
+        {text:"Something",link:"/"},
+    ]
+    const NavbarList2 = [
+        {text:"Something",link:"/about"},
+        {text:"Something",link:"/"},
+        {text:"Something",link:"/about"},
+        {text:"Something",link:"/"},
+        {text:"Login",link:"/"}
+    ]
     return (
         <div className="w-full px-10 min-h-16 py-3 sticky top-0 z-50">
             <div className="flex justify-between bg-[#9f9f9f55] backdrop-blur-lg items-center h-16 rounded-md">
@@ -17,7 +30,8 @@ function Navbar() {
                     <h1 className="text-xl">SurawittaNowath</h1>
                 </div>
                 <ul className="hidden lg:flex cursor-pointer h-full ">
-                    <motion.a
+                    {NavbarList.map((items) => (
+                        <motion.a
                         whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 1 }}
                         transition={{
@@ -30,65 +44,12 @@ function Navbar() {
                     >
                         <Link
                             className="w-full h-full flex items-center"
-                            to="/"
+                            to={items.link}
                         >
-                            Something
+                            {items.text}
                         </Link>
                     </motion.a>
-                    <motion.a
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 400,
-                            damping: 10,
-                            duration: 3,
-                        }}
-                        className="px-5 flex items-center hover:bg-gray-200 transition-colors duration-300 rounded-md"
-                    >
-                        <Link
-                            className="w-full h-full flex items-center"
-                            to="/about"
-                        >
-                            Something
-                        </Link>
-                    </motion.a>
-                    <motion.a
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 400,
-                            damping: 10,
-                            duration: 3,
-                        }}
-                        className="px-5 flex items-center hover:bg-gray-200 transition-colors duration-300 rounded-md"
-                    >
-                        <Link
-                            className="w-full h-full flex items-center"
-                            to="/"
-                        >
-                            Something
-                        </Link>
-                    </motion.a>
-                    <motion.a
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 400,
-                            damping: 10,
-                            duration: 3,
-                        }}
-                        className="px-5 flex items-center hover:bg-gray-200 transition-colors duration-300 rounded-md"
-                    >
-                        <Link
-                            className="w-full h-full flex items-center"
-                            to="/about"
-                        >
-                            Something
-                        </Link>
-                    </motion.a>
+                    ))}
                 </ul>
                 <div className="hidden lg:flex cursor-pointer h-full">
                     <motion.a
@@ -130,50 +91,17 @@ function Navbar() {
                     } flex dropdown absolute top-20 left-0 w-full transition-all`}
                 >
                     <ul className="w-full flex flex-col bg-white rounded-lg">
-                        <li className="w-full">
-                            <Link
-                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
-                                to="/about"
-                            >
-                                Something
-                            </Link>
-                        </li>
-                        <hr />
-                        <li className="w-full">
-                            <Link
-                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
-                                to="/about"
-                            >
-                                Something
-                            </Link>
-                        </li>
-                        <hr />
-                        <li className="w-full">
-                            <Link
-                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
-                                to="/about"
-                            >
-                                Something
-                            </Link>
-                        </li>
-                        <hr />
-                        <li className="w-full">
-                            <Link
-                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
-                                to="/"
-                            >
-                                Something
-                            </Link>
-                        </li>
-                        <hr />
-                        <li className="w-full">
-                            <Link
-                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
-                                to="/about"
-                            >
-                                Login
-                            </Link>
-                        </li>
+                        {NavbarList2.map((items) => (
+                            <li className="w-full">
+                                <Link
+                                    className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
+                                    onClick={(e) => HandelClick()}
+                                    to={items.link}
+                                >
+                                    {items.text}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
