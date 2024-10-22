c46ea0415a2342f4c5f1fa5eafbbc42546344ea3
Nowath <83205731+Nowath@users.noreply.github.com>
2024-10-22 12:10:16 +0700

    Navbar_Com

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,25 @@ import SWKLOGO from "../assets/SWKLOGO.png";
 import { motion, spring } from "framer-motion";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import { ChevronDown } from "lucide-react";
 
 function Navbar() {
     const [openNav, setOpenNav] = useState(false);
+    const [openDrop1, setOpenDrop1] = useState(false);
+    const [openDrop2, setOpenDrop2] = useState(false);
     function HandelClick() {
         setOpenNav(!openNav);
     }
+    function HandelClick1() {
+        setOpenDrop1(!openDrop1);
+    }
+    function HandelClick2() {
+        setOpenDrop2(!openDrop2);
+    }
     const NavbarList = [
-        {text:"Something",link:"/about"},
-        {text:"Something",link:"/"},
-        {text:"Something",link:"/about"},
-        {text:"Something",link:"/"},
-    ]
-    const NavbarList2 = [
-        {text:"Something",link:"/about"},
-        {text:"Something",link:"/"},
-        {text:"Something",link:"/about"},
-        {text:"Something",link:"/"},
-        {text:"Login",link:"/"}
-    ]
+        { text: "Home", link: "/" },
+        { text: "การประเมิน ITA ปี 2567", link: "/about" },
+    ];
     return (
         <div className="w-full px-10 min-h-16 py-3 sticky top-0 z-50">
             <div className="flex justify-between bg-[#9f9f9f55] backdrop-blur-lg items-center h-16 rounded-md">
@@ -32,24 +32,126 @@ function Navbar() {
                 <ul className="hidden lg:flex cursor-pointer h-full ">
                     {NavbarList.map((items) => (
                         <motion.a
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 400,
-                            damping: 10,
-                            duration: 3,
-                        }}
-                        className="px-5 flex items-center hover:bg-gray-200 transition-colors duration-300 rounded-md"
-                    >
-                        <Link
-                            className="w-full h-full flex items-center"
-                            to={items.link}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 1 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 400,
+                                damping: 10,
+                                duration: 3,
+                            }}
+                            className=" flex items-center hover:bg-gray-200 transition-colors duration-300 rounded-md"
                         >
-                            {items.text}
-                        </Link>
-                    </motion.a>
+                            <Link
+                                className="w-full px-5 h-full flex items-center"
+                                to={items.link}
+                            >
+                                {items.text}
+                            </Link>
+                        </motion.a>
                     ))}
+                    <div className=" select-none relative h-full">
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 1 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 400,
+                                damping: 10,
+                                duration: 3,
+                            }}
+                            className=" flex items-center h-full hover:bg-gray-200 transition-colors duration-300 rounded-md"
+                        >
+                            <div
+                                className="w-full px-5 h-full flex items-center"
+                                onClick={(e) => HandelClick1()}
+                            >
+                                ข้อมูลพื้นฐานของโรงเรียน
+                                <div
+                                    className={`${
+                                        openDrop1 ? `rotate-180` : ``
+                                    } transition-all`}
+                                >
+                                    <ChevronDown />
+                                </div>
+                            </div>
+                        </motion.div>
+                        <div
+                            className={`${
+                                openDrop1
+                                    ? `opacity-100 top-20`
+                                    : `opacity-0 top-[-3rem]`
+                            } flex flex-col absolute left-0 transition-all rounded-md overflow-hidden bg-white w-full`}
+                            onClick={(e) => HandelClick1()}
+                        >
+                            <Link className=" px-4 py-4" to="/about">
+                                <h1>ผู้บริหารโรงเรียนสุรวิทยาคาร</h1>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className=" select-none relative h-full">
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 1 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 400,
+                                damping: 10,
+                                duration: 3,
+                            }}
+                            className=" flex items-center h-full hover:bg-gray-200 transition-colors duration-300 rounded-md"
+                        >
+                            <div
+                                className="w-full px-5 h-full flex items-center"
+                                onClick={(e) => HandelClick2()}
+                            >
+                                ห้องเรียนออนไลน์
+                                <div
+                                    className={`${
+                                        openDrop2 ? `rotate-180` : ``
+                                    } transition-all`}
+                                >
+                                    <ChevronDown />
+                                </div>
+                            </div>
+                        </motion.div>
+                        <div
+                            className={`${
+                                openDrop2
+                                    ? `opacity-100 top-20`
+                                    : `opacity-0 top-[-20rem]`
+                            } flex flex-col absolute left-0 transition-all rounded-md overflow-hidden bg-white w-[120%]`}
+                            onClick={(e) => HandelClick2()}
+                        >
+                            <a
+                                className=" px-4 py-4"
+                                href="http://krukawita.sura.ac.th/"
+                            >
+                                <h1>ครูกวิตา อินธิสาร</h1>
+                            </a>
+                            <hr />
+                            <a
+                                className=" px-4 py-4"
+                                href="http://krupaphada.sura.ac.th/"
+                            >
+                                <h1>ครูปภาดา เจียมเมืองปัก</h1>
+                            </a>
+                            <hr />
+                            <a
+                                className=" px-4 py-4"
+                                href="http://kruwatchara.sura.ac.th/"
+                            >
+                                <h1>ครูวัชระ โกติรัมย์</h1>
+                            </a>
+                            <hr />
+                            <a
+                                className=" px-4 py-4"
+                                href="http://kruparnpitcha.sura.ac.th/e-learning/"
+                            >
+                                <h1>ครูปารย์พิชชา ชีวาพรไพศาล</h1>
+                            </a>
+                        </div>
+                    </div>
                 </ul>
                 <div className="hidden lg:flex cursor-pointer h-full">
                     <motion.a
@@ -91,7 +193,7 @@ function Navbar() {
                     } flex dropdown absolute top-20 left-0 w-full transition-all`}
                 >
                     <ul className="w-full flex flex-col bg-white rounded-lg">
-                        {NavbarList2.map((items) => (
+                        {NavbarList.map((items) => (
                             <li className="w-full">
                                 <Link
                                     className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
@@ -102,6 +204,15 @@ function Navbar() {
                                 </Link>
                             </li>
                         ))}
+                        <li className="w-full">
+                            <Link
+                                className=" w-full flex justify-center items-center h-16 hover:bg-slate-200 transition-all rounded-lg"
+                                onClick={(e) => HandelClick()}
+                                to="/"
+                            >
+                                Login
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
